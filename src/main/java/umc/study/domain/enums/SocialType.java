e10b41b4b17d25534caeda0a4de1b03d84758e15
MINSOOM <134378012+sooominie@users.noreply.github.com>
2025-05-09 01:05:11 +0900

    임시 커밋: 브랜치 이동 전 작업 저장

---
+++ src/main/java/umc/study/domain/enums/SocialType.java
@@ -0,0 +1,5 @@
+package umc.study.domain.enums;
+
+public enum SocialType {
+    KAKAP,NAVER,APPLE,GOOGLE,FACEBOOK
+}
--- src/test/java/umc/ApplicationTests.java
+++ src/test/java/umc/ApplicationTests.java
@@ -1,4 +1,4 @@
-package umc.spring;
+package umc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
